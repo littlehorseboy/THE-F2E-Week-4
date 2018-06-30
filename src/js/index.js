cd3c0ea90d1a0292aa3ee85f97ab540519cf43a6
littlehorseboy <littlehorseboy@gmail.com>
2018-07-01 00:21:06 +0800

    product details

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,46 @@
 Object.defineProperty(Vue.prototype, '$faker', { value: faker });
 
-window.sr = ScrollReveal();
-sr.reveal('.image-Container');
+const Home = Vue.component('Home', {
+  template: '#Home',
+  data() {
+    return {
+      
+    };
+  },
+  mounted() {
+    window.sr = ScrollReveal();
+    sr.reveal('.image-Container');
+  },
+});
+
+const Product = {
+  template: '#Product',
+  computed: {
+    product() {
+      return this.$store.getters.getProductById(this.$route.params.id);
+    },
+  },
+};
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+  },
+  {
+    path: '/product/:id',
+    name: 'product',
+    component: Product,
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+const app = new Vue({
+  el: '#app',
+  router,
+  store,
+});
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,54 @@
 const store = new Vuex.Store({
   state: {
-    
+    products: [
+      {
+        id: '01',
+        productName: 'LINEN BLAZER',
+        subtitle: 'Men’s outfit',
+        discription: 'In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among the crew. Now hands are wanted here, and then again hands are wanted there. There is no staying in any one place; for at one and the same time everything has to be done everywhere. It is much the same with him who endeavors the description of the scene.',
+      },
+      {
+        id: '02',
+        productName: 'FREELIFT',
+        subtitle: 'Men’s basics',
+        discription: 'In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among the crew. Now hands are wanted here, and then again hands are wanted there. There is no staying in any one place; for at one and the same time everything has to be done everywhere. It is much the same with him who endeavors the description of the scene.',
+      },
+      {
+        id: '03',
+        productName: 'SUPER SKINNY',
+        subtitle: 'Men’s pattern shirts',
+        discription: 'In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among the crew. Now hands are wanted here, and then again hands are wanted there. There is no staying in any one place; for at one and the same time everything has to be done everywhere. It is much the same with him who endeavors the description of the scene.',
+      },
+      {
+        id: '04',
+        productName: 'DENIM',
+        subtitle: 'Men’s cadual',
+        discription: 'In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among the crew. Now hands are wanted here, and then again hands are wanted there. There is no staying in any one place; for at one and the same time everything has to be done everywhere. It is much the same with him who endeavors the description of the scene.',
+      },
+      {
+        id: '05',
+        productName: 'SWEATSHIRTS',
+        subtitle: 'Men’s jacket',
+        discription: 'In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among the crew. Now hands are wanted here, and then again hands are wanted there. There is no staying in any one place; for at one and the same time everything has to be done everywhere. It is much the same with him who endeavors the description of the scene.',
+      },
+      {
+        id: '06',
+        productName: 'VINTAGE DENIM',
+        subtitle: 'Men’s classic',
+        discription: 'In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among the crew. Now hands are wanted here, and then again hands are wanted there. There is no staying in any one place; for at one and the same time everything has to be done everywhere. It is much the same with him who endeavors the description of the scene.',
+      },
+      {
+        id: '07',
+        productName: 'EDITION',
+        subtitle: 'Men’s shirts',
+        discription: 'In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among the crew. Now hands are wanted here, and then again hands are wanted there. There is no staying in any one place; for at one and the same time everything has to be done everywhere. It is much the same with him who endeavors the description of the scene.',
+      },
+    ],
   },
   getters: {
-    
+    getProductById: (state) => (id) => {
+      return state.products.find(product => product.id === id);
+    },
   },
   actions: {
     
